0b52cd1d123a4f424175e830510ab109c7fef6db
xav-db <xav@helix-db.com>
2025-07-10 12:17:56 +0100

    fixing warnings

--- helixdb/src/helix_engine/graph_core/traversal_iter.rs
+++ helixdb/src/helix_engine/graph_core/traversal_iter.rs
@@ -5,7 +5,6 @@ use heed3::{RoTxn, RwTxn};
 use super::ops::tr_val::TraversalVal;
 use crate::{
     helix_engine::{storage_core::storage_core::HelixGraphStorage, types::GraphError},
-    helixc::generator::utils::Order,
     protocol::{filterable::Filterable, value::Value},
 };
 use itertools::Itertools;
