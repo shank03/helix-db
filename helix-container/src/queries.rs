6f1f5dd354773662f58f6b95cea543261576ceba
xav-db <xav@helix-db.com>
2025-07-13 11:37:08 +0100

    fixing problem with for loops access and edge id access.

--- helix-container/src/queries.rs
+++ helix-container/src/queries.rs
@@ -59,23 +59,90 @@ use std::collections::{HashMap, HashSet};
 use std::sync::Arc;
 use std::time::Instant;
 
-pub struct File15 {
-    pub name: String,
-    pub age: i32,
+pub struct Chapter {
+    pub chapter_index: i64,
 }
 
-pub struct Follows {
-    pub from: File15,
-    pub to: File15,
+pub struct SubChapter {
+    pub title: String,
+    pub content: String,
+}
+
+pub struct Contains {
+    pub from: Chapter,
+    pub to: SubChapter,
+}
+
+pub struct EmbeddingOf {
+    pub from: SubChapter,
+    pub to: Embedding,
+    pub chunk: String,
+}
+
+pub struct Embedding {
+    pub chunk: String,
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct file15_3Input {
-    pub userID: ID,
+pub struct searchdocs_ragInput {
+    pub query: Vec<f64>,
+    pub k: i32,
 }
 #[handler]
-pub fn file15_3(input: &HandlerInput, response: &mut Response) -> Result<(), GraphError> {
-    let data: file15_3Input = match sonic_rs::from_slice(&input.request.body) {
+pub fn searchdocs_rag(input: &HandlerInput, response: &mut Response) -> Result<(), GraphError> {
+    let data: searchdocs_ragInput = match sonic_rs::from_slice(&input.request.body) {
+        Ok(data) => data,
+        Err(err) => return Err(GraphError::from(err)),
+    };
+
+    let mut remapping_vals = RemappingMap::new();
+    let db = Arc::clone(&input.graph.storage);
+    let txn = db.graph_env.read_txn().unwrap();
+    let vecs = G::new(Arc::clone(&db), &txn)
+        .search_v::<fn(&HVector, &RoTxn) -> bool>(&data.query, data.k as usize, None)
+        .collect_to::<Vec<_>>();
+    let subchapters = G::new_from(Arc::clone(&db), &txn, vecs.clone())
+        .in_("EmbeddingOf", &EdgeType::Node)
+        .collect_to::<Vec<_>>();
+    let mut return_vals: HashMap<String, ReturnValue> = HashMap::new();
+    return_vals.insert("subchapters".to_string(), ReturnValue::from_traversal_value_array_with_mixin(G::new_from(Arc::clone(&db), &txn, subchapters.clone())
+
+.map_traversal(|item, txn| { traversal_remapping!(remapping_vals, item.clone(), "title" => G::new_from(Arc::clone(&db), &txn, vec![item.clone()])
+
+.check_property("title").collect_to::<Vec<_>>())?;
+traversal_remapping!(remapping_vals, item.clone(), "content" => G::new_from(Arc::clone(&db), &txn, vec![item.clone()])
+
+.check_property("content").collect_to::<Vec<_>>())?;
+ Ok(item) }).collect_to::<Vec<_>>().clone(), remapping_vals.borrow_mut()));
+
+    txn.commit().unwrap();
+    response.body = sonic_rs::to_vec(&return_vals).unwrap();
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct chunksData {
+    pub vector: Vec<f64>,
+    pub chunk: String,
+}
+#[derive(Serialize, Deserialize)]
+pub struct subchaptersData {
+    pub chunks: Vec<chunksData>,
+    pub title: String,
+    pub content: String,
+}
+#[derive(Serialize, Deserialize)]
+pub struct chaptersData {
+    pub id: i64,
+    pub subchapters: Vec<subchaptersData>,
+}
+#[derive(Serialize, Deserialize)]
+pub struct loaddocs_ragInput {
+    pub chapters: Vec<chaptersData>,
+}
+#[handler]
+pub fn loaddocs_rag(input: &HandlerInput, response: &mut Response) -> Result<(), GraphError> {
+    let data: loaddocs_ragInput = match sonic_rs::from_slice(&input.request.body) {
         Ok(data) => data,
         Err(err) => return Err(GraphError::from(err)),
     };
@@ -83,35 +150,54 @@ pub fn file15_3(input: &HandlerInput, response: &mut Response) -> Result<(), Gra
     let mut remapping_vals = RemappingMap::new();
     let db = Arc::clone(&input.graph.storage);
     let mut txn = db.graph_env.write_txn().unwrap();
-    Drop::<Vec<_>>::drop_traversal(
-        G::new(Arc::clone(&db), &txn)
-            .n_from_id(&data.userID)
-            .collect_to_obj(),
-        Arc::clone(&db),
-        &mut txn,
-    )?;
-    let mut return_vals: HashMap<String, ReturnValue> = HashMap::new();
-    txn.commit().unwrap();
-    response.body = sonic_rs::to_vec(&return_vals).unwrap();
-    Ok(())
-}
-
-#[handler]
-pub fn file15(input: &HandlerInput, response: &mut Response) -> Result<(), GraphError> {
-    let mut remapping_vals = RemappingMap::new();
-    let db = Arc::clone(&input.graph.storage);
-    let mut txn = db.graph_env.write_txn().unwrap();
-    Drop::<Vec<_>>::drop_traversal(
-        G::new(Arc::clone(&db), &txn)
-            .n_from_type("File15")
-            .collect_to::<Vec<_>>(),
-        Arc::clone(&db),
-        &mut txn,
-    )?;
+    for chaptersData { id, subchapters } in data.chapters {
+        let chapter_node = G::new_mut(Arc::clone(&db), &mut txn)
+            .add_n("Chapter", Some(props! { "chapter_index" => &id }), None)
+            .collect_to_obj();
+        for subchaptersData {
+            title,
+            content,
+            chunks,
+        } in subchapters
+        {
+            let subchapter_node = G::new_mut(Arc::clone(&db), &mut txn)
+                .add_n(
+                    "SubChapter",
+                    Some(props! { "title" => &title, "content" => &content }),
+                    None,
+                )
+                .collect_to_obj();
+            G::new_mut(Arc::clone(&db), &mut txn)
+                .add_e(
+                    "Contains",
+                    None,
+                    chapter_node.id(),
+                    subchapter_node.id(),
+                    true,
+                    EdgeType::Node,
+                )
+                .collect_to_obj();
+            for chunksData { chunk, vector } in chunks {
+                let vec = G::new_mut(Arc::clone(&db), &mut txn)
+                    .insert_v::<fn(&HVector, &RoTxn) -> bool>(&vector, "Embedding", None)
+                    .collect_to_obj();
+                G::new_mut(Arc::clone(&db), &mut txn)
+                    .add_e(
+                        "EmbeddingOf",
+                        Some(props! { "chunk" => chunk.clone() }),
+                        subchapter_node.id(),
+                        vec.id(),
+                        true,
+                        EdgeType::Node,
+                    )
+                    .collect_to_obj();
+            }
+        }
+    }
     let mut return_vals: HashMap<String, ReturnValue> = HashMap::new();
     return_vals.insert(
-        "success".to_string(),
-        ReturnValue::from(Value::from("success")),
+        "Success".to_string(),
+        ReturnValue::from(Value::from("Success")),
     );
 
     txn.commit().unwrap();
@@ -120,28 +206,32 @@ pub fn file15(input: &HandlerInput, response: &mut Response) -> Result<(), Graph
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct file15_2Input {
-    pub userID: ID,
+pub struct edge_nodeInput {
+    pub id: ID,
 }
 #[handler]
-pub fn file15_2(input: &HandlerInput, response: &mut Response) -> Result<(), GraphError> {
-    let data: file15_2Input = match sonic_rs::from_slice(&input.request.body) {
+pub fn edge_node(input: &HandlerInput, response: &mut Response) -> Result<(), GraphError> {
+    let data: edge_nodeInput = match sonic_rs::from_slice(&input.request.body) {
         Ok(data) => data,
         Err(err) => return Err(GraphError::from(err)),
     };
 
     let mut remapping_vals = RemappingMap::new();
     let db = Arc::clone(&input.graph.storage);
-    let mut txn = db.graph_env.write_txn().unwrap();
-    Drop::<Vec<_>>::drop_traversal(
-        G::new(Arc::clone(&db), &txn)
-            .n_from_id(&data.userID)
-            .out("Follows", &EdgeType::Node)
-            .collect_to::<Vec<_>>(),
-        Arc::clone(&db),
-        &mut txn,
-    )?;
+    let txn = db.graph_env.read_txn().unwrap();
+    let e = G::new(Arc::clone(&db), &txn)
+        .n_from_type("Chapter")
+        .out_e("Contains")
+        .collect_to::<Vec<_>>();
     let mut return_vals: HashMap<String, ReturnValue> = HashMap::new();
+    return_vals.insert(
+        "e".to_string(),
+        ReturnValue::from_traversal_value_array_with_mixin(
+            e.clone().clone(),
+            remapping_vals.borrow_mut(),
+        ),
+    );
+
     txn.commit().unwrap();
     response.body = sonic_rs::to_vec(&return_vals).unwrap();
     Ok(())
