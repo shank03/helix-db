a88614be09cf5ea9d403c9c4288db8e195441925
xav-db <xav@helix-db.com>
2025-07-16 14:38:47 +0100

    pushing fixes to cli and redeploy

--- helix-cli/src/main.rs
+++ helix-cli/src/main.rs
@@ -1418,7 +1418,7 @@ fi
                 );
             }
 
-            let key = github_login().await.unwrap();
+            let (key, user_id) = github_login().await.unwrap();
             println!("{}", "Successfully logged in!".green().bold());
 
             let mut cred_file = OpenOptions::new()
@@ -1429,7 +1429,7 @@ fi
                 .open(cred_path)
                 .unwrap();
 
-            if let Err(e) = cred_file.write_all(&format!("helix_user_key={key}").into_bytes()) {
+            if let Err(e) = cred_file.write_all(&format!("helix_user_id={user_id}\nhelix_user_key={key}").into_bytes()) {
                 println!(
                     "Got error when writing key: {}\nYou're key is: {}",
                     e.to_string().red(),
