2c1e2d90842fc20febd14ab3aa69e3737e4f3a18
xav-db <xav@helix-db.com>
2025-08-17 12:54:20 +0100

    fixing test

--- hql-tests/tests/index_with_default/queries.hx
+++ hql-tests/tests/index_with_default/queries.hx
@@ -7,8 +7,8 @@ N::File9 {
 
 
 QUERY file9(name: String, id: ID) =>
-    user <- N<File9>({name: name, age: 10, other_field: "other_field"})
-    user <- N<File9>(id)
+    user <- AddN<File9>({name: name, age: 10, other_field: "other_field"})
+    user_by_id <- N<File9>(id)
     node <- N<File9>({name: name})
     node_by_name <- N<File9>({count: 24.5})
     RETURN user, node, node_by_name
