1bf74d96992463a7fd849e597fa542635c063d9a
Xav <104228340+xav-db@users.noreply.github.com>
2025-08-22 14:45:48 +0100

    Revert "fix(analyzer): fixing panic on missing type"

--- helix-db/src/helixc/analyzer/methods/query_validation.rs
+++ helix-db/src/helixc/analyzer/methods/query_validation.rs
@@ -71,9 +71,12 @@ pub(crate) fn validate_query<'a>(ctx: &mut Ctx<'a>, original_query: &'a Query) {
         );
     }
     for stmt in &original_query.statements {
-        match validate_statements(ctx, &mut scope, original_query, &mut query, stmt) {
-            Some(s) => query.statements.push(s),
-            None => return,
+        let statement = validate_statements(ctx, &mut scope, original_query, &mut query, stmt);
+        if let Some(s) = statement {
+            query.statements.push(s);
+        } else {
+            // given all erroneous statements are caught by the analyzer, this should never happen
+            unreachable!()
         }
     }
 
