32b156c2b5e90bbe54a9c5931993d90bf924b7a6
xav-db <xav@helix-db.com>
2025-08-27 14:40:34 +0100

    clippy

--- helix-db/src/protocol/return_values.rs
+++ helix-db/src/protocol/return_values.rs
@@ -2,7 +2,7 @@ use super::{
     remapping::{Remapping, ResponseRemapping},
     value::Value,
 };
-use crate::helix_engine::traversal_core::traversal_value::TraversalValue;
+use crate::{debug_println, helix_engine::traversal_core::traversal_value::TraversalValue};
 use crate::utils::{
     count::Count,
     filterable::{Filterable, FilterableType},
@@ -172,11 +172,11 @@ impl ReturnValue {
     /// ```
     #[inline(always)]
     pub fn mixin_remapping(self, remappings: HashMap<String, Remapping>) -> Self {
-        println!("Remapping: {:#?}", self);
+        debug_println!("Remapping: {:#?}", self);
         let return_value = match self {
             ReturnValue::Object(mut a) => {
                 remappings.into_iter().for_each(|(k, v)| {
-                    println!("k: {:?}, v: {:?}", k, v);
+                    debug_println!("k: {:?}, v: {:?}", k, v);
                     if v.exclude {
                         let _ = a.remove(&k);
                     } else if let Some(new_name) = v.new_name {
@@ -193,7 +193,7 @@ impl ReturnValue {
             }
             _ => unreachable!(),
         };
-        println!("Return value: {:?}", return_value);
+        debug_println!("Return value: {:return_value?}");
         return_value
     }
 
